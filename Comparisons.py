e094c5c59e3334421bf2ce0194ad2ac4e763d0d1
sumanthpydi <scs780664@kgpian.iitkgp.ac.in>
2026-03-29 23:03:19 +0530

    Update Comparisons.py
    Signed-off-by: sumanthpydi <scs780664@kgpian.iitkgp.ac.in>

--- Comparisons.py
+++ Comparisons.py
@@ -116,7 +116,7 @@ if __name__ == "__main__":
             recovered_inputs_3chnl,
             evnt_frame,
             eps_dbscan=15,
-            eps_spydi=12,
+            eps_spydi=11,
             min_samples_val=10,
             mindiagonalsquared=2300
         )
